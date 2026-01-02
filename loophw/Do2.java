54f7acdac80fd32c7501bf692a81215e79c81c4c
jeyapandikanna-gif <jeyapandikannan@gmail.com>
2026-01-02 14:22:50 +0530

    Add files via upload

---
+++ loophw/Do2.java
@@ -0,0 +1,20 @@
+class Do2
+{
+	public static void main(String []args)
+	{
+		int i=1;
+		do
+		{
+			if(i%2==0)
+			{
+		
+			System.out.println(i);
+			}
+		i++;
+			
+		}
+		while(i<=30);
+	
+	}
+	
+}
